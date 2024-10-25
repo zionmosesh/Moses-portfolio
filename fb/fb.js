676898c77d74b8adb2921ba337c84db5d12e21d7
zionmosesh <52451645+zionmosesh@users.noreply.github.com>
2024-10-25 11:21:13 +0100

    first commit

---
+++ fb/fb.js
@@ -0,0 +1,20 @@
+var database = [
+    {
+        username: "moses"
+        password: "supersecret"
+    }
+    ];
+    
+    var newsFeed = [
+    {
+        username: "Bobby"
+        timeline: "So tired from that learning"
+    },
+    {
+        username: "Sally"
+        timeline: "JavaScript is sooo cooool"
+    }
+    ];
+    
+    var userNameprompt = prompt("What's your username?");
+    var passwordPrompt = prompt("What's your password?");
